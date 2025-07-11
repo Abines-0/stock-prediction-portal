7c378f12c0ac95ac170da20973f687add55de25e
Abines0 <abineshan356@gmail.com>
2025-07-11 10:20:11 +0530

    registration page

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -8,7 +8,7 @@ const Main = () => {
             <div className='p-5 text-center bg-light-dark rounded'>
                 <h1 className='text-light'>stock prediction portal</h1>
                 <p className='text-light lead'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio eius aspernatur ducimus vitae. Mollitia itaque porro, nisi earum laborum, explicabo at autem quidem adipisci dolorum velit rerum dignissimos sed molestias.</p>
-                <Button text='Login'class='btn-outline-info'/>
+                <Button text='Login' class='btn-outline-info'/>
             </div>
         </div>
     </>
